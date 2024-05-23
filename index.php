03a6f0600b7e3e56a986ea45b332dba6add761d0
n2oliver <silva.liver@gmail.com>
2024-05-23 20:17:24 -0300

    ajusta rodape

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
         <div id="cursos-container" class="section content"></div>
     </section>
     <footer style="text-align: center; color: white; padding: 24px; background-image: linear-gradient(transparent,#000000)">
-        <span style="font-family: Montserrat">Oliv3r Store</span> - Afiliação Amazon, Hotmart e Hinode<br/>
+        <span style="font-family: Montserrat">Oliv3r Store</span>
         <span style="font-size: 9px">Todos os direitos reservados - Oliv3r Store -  2024</span>
         <span style="font-size: 9px">CNPJ 54.983.228/0001-78</span>
     </footer>
