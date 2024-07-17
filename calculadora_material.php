2b35538220ad50b8350b3c21bda7a7efca4c1c4d
ensaioseletricos <ensaioseletricostecnologia@gmail.com>
2024-07-17 10:38:23 -0300

    adiciona melhoria ao peça generica

--- calculadora_material.php
+++ calculadora_material.php
@@ -68,22 +68,57 @@
         .tooltip-icon:focus + .tooltip-text {
             display: block;
         }
-        input[type="number"], input[type="submit"] {
-            width: -moz-available;
+        input[type="number"], input[type="submit"], button {
+            width: 100%;
             padding: 10px;
             margin-bottom: 10px;
             border: 1px solid #ccc;
             border-radius: 4px;
             font-size: 16px;
         }
-        input[type="submit"] {
+        input[type="submit"], button {
             background-color: #28a745;
             color: white;
             cursor: pointer;
             border: none;
         }
-        input[type="submit"]:hover {
+        input[type="submit"]:hover, button:hover {
             background-color: #218838;
+        }
+        #measuresContainer {
+            display: flex;
+            flex-direction: row;
+            overflow-x: scroll;
+        }
+        #addMeasure {
+            border-radius: 10px;
+            width: auto;
+            padding: 14px;
+            height: 45px;
+            line-height: 50%;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 14px;
+            font-weight: 700;
+            margin-top: 14px;
+        }
+        .measure-item {
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+            min-width: 340px;
+            margin-right: 20px;
+        }
+        .measure-item img {
+            width: 60px;
+            margin-top: 10px;
+        }
+        .measure-fields {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        .measure-fields input {
+            width: 100px;
         }
         .result {
             background: #e9ecef;
@@ -93,6 +128,10 @@
         }
         .navbar {
             z-index: 2;
+            width: 100%;
+        }
+        footer {
+            width: 100%;
         }
     </style>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
@@ -100,48 +139,28 @@
         crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="styles-index.css">
 </head>
 
 <body>
 <?php @include("video-background.php"); ?>
 
     <div class="container col-md-8 col-sm-12 col-lg-8">
-
-<div class="row">
-    <?php @include("navbar.php"); ?>
-</div>
+        <div class="row">
+            <?php @include("navbar.php"); ?>
+        </div>
         <h1>Calculadora de Material com Perdas</h1>
-        <form method="post" action="">
-            <div class="form-group">
-                <label for="area">Superfície (m²):</label>
-                <span class="tooltip-icon" tabindex="0">?</span>
-                <span class="tooltip-text">Superfície onde o material será utilizado (m²).</span>
-                <br/>
-                <input type="number" step="0.01" id="area" name="area" required>
-            </div>
-            
-            <div class="form-group">
-                <label for="cobertura">Área coberta por uma unidade do material (m²/unidade):</label>
-                <span class="tooltip-icon" tabindex="0">?</span>
-                <span class="tooltip-text">Área que uma unidade do material pode cobrir.</span>
-                <br/>
-                <input type="number" step="0.01" id="cobertura" name="cobertura" required>
-            </div>
-            
-            <div class="form-group">
-                <label for="perda">Fator de perda (%):</label>
-                <span class="tooltip-icon" tabindex="0">?</span>
-                <span class="tooltip-text">Insira o percentual de perda esperado durante a aplicação do material.</span>
-                <br/>
-                <input type="number" step="0.01" id="perda" name="perda" required>
-            </div>
-            <input type="submit" name="submit" value="Calcular">
-        </form>
+        
 
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $area = floatval($_POST['area']);
+            $larguras = $_POST['largura'];
+            $comprimentos = $_POST['comprimento'];
+            $area_total = 0;
+
+            for ($i = 0; $i < count($larguras); $i++) {
+                $area_total += floatval($larguras[$i]) * floatval($comprimentos[$i]);
+            }
+
             $cobertura = floatval($_POST['cobertura']);
             $perda = floatval($_POST['perda']);
 
@@ -149,14 +168,57 @@
             $fator_perda = $perda / 100;
 
             // Calculando a quantidade de material necessária
-            $quantidade_material = $area / ($cobertura * (1 - $fator_perda));
+            $quantidade_material = $area_total / ($cobertura * (1 - $fator_perda));
 
             echo "<div class='result'>";
             echo "<h2>Resultado:</h2>";
+            echo "<p>Área total a ser coberta: " . number_format($area_total, 2) . " m²</p>";
             echo "<p>Quantidade de material necessária: " . number_format($quantidade_material, 2) . " unidades</p>";
             echo "</div>";
         }
         ?>
+        <form id="calcForm" method="post" action="">
+            <div id="measuresContainer">
+                <div class="measure-item">
+                    <div class="measure-fields">
+                        <div class="row">
+                            <div class="form-group col-sm-6 col-md-6 col-lg-6">
+                                <label for="nome">Nome da peça:</label><br/>
+                                <input type="text" class="nome form-control" name="nome[]" required>
+                            </div>
+                            <div class="form-group col-sm-6 col-md-6 col-lg-6">
+                                <label for="largura">Largura (m):</label><br/>
+                                <input type="number" step="0.01" class="largura" name="largura[]" required>
+                            </div>
+                            <div class="form-group col-sm-6 col-md-6 col-lg-6">
+                                <label for="comprimento">Comprimento (m):</label><br/>
+                                <input type="number" step="0.01" class="comprimento" name="comprimento[]" required>
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6" style="flex: 0">
+                                <img alt="Ícone da peça" src="/img/peca-generica.png" style="margin-top: 10px">
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn-danger remover">Remover</button>
+                    </div>
+                </div>
+            </div>
+            <button type="button" id="addMeasure">Adicionar medida</button>
+            <div class="form-group">
+                <label for="cobertura">Área coberta por uma unidade do material (m²/unidade):</label>
+                <span class="tooltip-icon" tabindex="0">?</span>
+                <span class="tooltip-text">Área que uma unidade do material pode cobrir.</span>
+                <input type="number" step="0.01" id="cobertura" name="cobertura" required>
+            </div>
+            <div class="form-group">
+                <label for="perda">Fator de perda (%):</label>
+                <span class="tooltip-icon" tabindex="0">?</span>
+                <span class="tooltip-text">Insira o percentual de perda esperado durante a aplicação do material.</span>
+                <input type="number" step="0.01" id="perda" name="perda" required>
+            </div>
+            <input type="submit" name="submit" value="Calcular">
+        </form>
         
         <div class="col-lg-2 px-0"></div>
         <div class="row">
@@ -166,9 +228,49 @@
     </div>
     
     <script>
-        $(document).ready(()=>{
-            $('.nav-item').removeClass('active');
-            $('.nav-item:contains("Sobre mim")').addClass("active");
+        $(document).ready(function() {
+            $('input').on('keyup', function(e) {
+
+            })
+            $('.remover').unbind('click').on('click', function(e) {
+                if($('.measure-item').length !== 1) {
+                    $(e.target).parents('.measure-item').remove();
+                }
+            });
+            $('#addMeasure').click(function() {
+                $('#measuresContainer').append(`
+                    <div class="measure-item">
+                        <div class="measure-fields">
+                            <div class="row">
+                                <div class="form-group col-sm-6 col-md-6 col-lg-6">
+                                    <label for="nome">Nome da peça:</label><br/>
+                                    <input type="text" class="nome form-control" name="nome[]" required>
+                                </div>
+                                <div class="form-group col-sm-6 col-md-6 col-lg-6">
+                                    <label for="largura">Largura (m):</label><br/>
+                                    <input type="number" step="0.01" class="largura" name="largura[]" required>
+                                </div>
+                                <div class="form-group col-sm-6 col-md-6 col-lg-6">
+                                    <label for="comprimento">Comprimento (m):</label><br/>
+                                    <input type="number" step="0.01" class="comprimento" name="comprimento[]" required>
+                                </div>
+                                <div class="col-sm-6 col-md-6 col-lg-6" style="flex: 0">
+                                    <img alt="Ícone da peça" src="/img/peca-generica.png" width="100%" style="margin-top: 10px">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="btn-danger remover">Remover</button>
+                        </div>
+                    </div>
+                `);
+                
+                $('.remover').unbind('click').on('click', function(e) {
+                    if($('.measure-item').length !== 1) {
+                        $(e.target).parents('.measure-item').remove();
+                    }
+                });
+            });
         });
     </script>
 </body>
