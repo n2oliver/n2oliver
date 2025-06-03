b5b5e929ea352cf6f5d264a9efd5d462d0018e21
Óliver Silva <silva.liver@gmail.com>
2025-06-03 17:40:27 -0300

    melhora visual do footer

--- loja_virtual/index.php
+++ loja_virtual/index.php
@@ -90,11 +90,7 @@
             </div>
         </div>
     </section>
-    <footer style="text-align: center; color: white; padding: 24px; background-image: linear-gradient(transparent,#000000)">
-        <a href="/"><span style="font-family: Montserrat">Oliv3r Store</span></a>
-        <span style="font-size: 9px">Todos os direitos reservados - Oliv3r Store -  2024</span>
-        <span style="font-size: 9px">CNPJ 54.983.228/0001-78</span>
-    </footer>
+    <?php include('../footer.php'); ?>
 </div>
 <div class="col-lg-2 px-0"></div>
 </div>
