c5fe11d3715b895f83a968d8c54102eca25aae39
Óliver Silva <silva.liver@gmail.com>
2025-06-27 23:30:45 -0300

    cookie consent com tarte au citron

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
     </div>
     <a href="/"><span class="oliver-dev-logo" style="font-size: 32px">n2oliver</span></a>
     <span style="font-size: 9px">Todos os direitos reservados - n2oliver -  2025</span>
-    <a href="/politica-de-privacidade.php">Política de Privacidade</a>
+    <a href="/politica-de-privacidade.html">Política de Privacidade</a>
     <span style="font-size: 9px">CNPJ 60.407.027/0001-25</span>
 </footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -22,13 +22,65 @@
 <script src="/limitar-largura-img-iframe.js">
 <link rel="stylesheet" href="sobre-mim.css"/>
 </script>
+<script src="/lib/tarteaucitron/tarteaucitron.js"></script>
 
-<?php include('politica-de-privacidade-aviso.php'); ?>
 <script>
-    if(localStorage.getItem('cookieConsent')) {
-        const adBanner = document.querySelector('#ad-banner');
-        if(adBanner) {
-            adBanner.outerHTML = `<a class="${adBanner.className}"href="https://n2oliver.com/loja_virtual" target="_blank">${adBanner.innerHTML}</a>`;
-        }
+tarteaucitron.init({
+    "privacyUrl": "", /* Privacy policy url */
+    "bodyPosition": "bottom", /* or top to bring it as first element for accessibility */
+
+    "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+    "cookieName": "tarteaucitron", /* Cookie name */
+
+    "orientation": "middle", /* Banner position (top - bottom - middle - popup) */
+
+    "groupServices": false, /* Group services by category */
+    "showDetailsOnClick": true, /* Click to expand the description */
+    "serviceDefaultState": "wait", /* Default state (true - wait - false) */
+
+    "showAlertSmall": false, /* Show the small banner on bottom right */
+    "cookieslist": false, /* Show the cookie list */
+    
+    "showIcon": true, /* Show cookie icon to manage cookies */
+    // "iconSrc": "", /* Optional: URL or base64 encoded image */
+    "iconPosition": "BottomRight", /* Position of the icon between BottomRight, BottomLeft, TopRight and TopLeft */
+
+    "adblocker": false, /* Show a Warning if an adblocker is detected */
+
+    "DenyAllCta" : true, /* Show the deny all button */
+    "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+    "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+    "alwaysNeedConsent": false, /* Ask the consent for "Privacy by design" services */
+    
+    "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+
+    "removeCredit": false, /* Remove credit link */
+    "moreInfoLink": true, /* Show more info link */
+    "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+    "useExternalJs": false, /* If false, the tarteaucitron.services.js file will be loaded */
+
+    // "cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for subdomain website */
+
+    "readmoreLink": "", /* Change the default readmore link pointing to tarteaucitron.io */
+    
+    "mandatory": true, /* Show a message about mandatory cookies */
+    "mandatoryCta": true, /* Show the disabled accept button when mandatory on */
+    
+    // "customCloserId": "", /* Optional a11y: Custom element ID used to open the panel */
+
+    "googleConsentMode": true, /* Enable Google Consent Mode v2 for Google ads and GA4 */
+    "bingConsentMode": true, /* Enable Bing Consent Mode for Clarity and Bing Ads */
+    "softConsentMode": false, /* Soft consent mode (consent is required to load the services) */
+
+    "dataLayer": false, /* Send an event to dataLayer with the services status */
+    "serverSide": false, /* Server side only, tags are not loaded client side */
+    
+    "partnersList": false /* Details the number of partners on the popup and middle banner */
+});
+</script>
+<script>
+    const adBanner = document.querySelector('#ad-banner');
+    if(adBanner) {
+        adBanner.outerHTML = `<a class="${adBanner.className}"href="https://n2oliver.com/loja_virtual" target="_blank">${adBanner.innerHTML}</a>`;
     }
 </script>
--- noads-footer.php
+++ noads-footer.php
@@ -1,7 +1,6 @@
 <footer class="mt-2 m-auto" style="display: flex; justify-content: center; flex-direction: column; text-align: center; color: white; padding: 24px; background-image: linear-gradient(transparent,#000000);">
     <a href="/"><span class="oliver-dev-logo" style="font-family: Ubuntu; font-size: 32px">n2oliver</span></a>
     <span style="font-size: 9px">Todos os direitos reservados - n2oliver -  2025</span>
-    <a href="/politica-de-privacidade.php">Política de Privacidade</a>
+    <a href="/politica-de-privacidade.html">Política de Privacidade</a>
     <span style="font-size: 9px">CNPJ 60.407.027/0001-25</span>
 </footer>
-<?php include('politica-de-privacidade-aviso.php'); ?>
