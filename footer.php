6d67425ec4bd1b8d45dc836c21c01a6a4cbd3b8d
Óliver Silva <silva.liver@gmail.com>
2025-08-11 18:11:28 -0300

    visibilidade do painel de cookies e linguagem

--- footer.php
+++ footer.php
@@ -101,14 +101,16 @@
 <script>
 window.gtranslateSettings = {"default_language":"pt","native_language_names":true,"languages":["pt","fr","it","es","en","zh-TW","zh-CN","nl"],"wrapper_selector":".gtranslate_wrapper"}
 window.addEventListener('load', function() {
-  var listOfOverlappingElements = ['gt_switcher_wrapper', 'tarteaucitronIconBottomRight'];
   var el;
-  for(let className of listOfOverlappingElements) {
-    el = document.querySelector(`.${className}`);
+  el = document.querySelector(`.gt_switcher_wrapper`);
 
-    if(el) {
-      el.style.setProperty('z-index', '9999', 'important');
-    }
+  if(el) {
+    el.style.setProperty('z-index', '9999', 'important');
+  }
+  
+  el = document.querySelector(`.tarteaucitronIconBottomRight`);
+  if(el) {
+    el.style.setProperty('z-index', '9999', 'important');
   }
 });
 </script>
@@ -116,7 +118,7 @@ window.addEventListener('load', function() {
 <div style="position: absolute; z-index: 99999">
   <input autocomplete="off" type="checkbox" id="aadsstickyme5l1sdx" hidden />
   <div style="padding-top: 0; padding-bottom: auto;">
-    <div style="width:100%;height:auto;position:fixed;text-align:center;font-size:0;top:0;left:0;right:0;margin:auto">
+    <div style="width:100%;height:auto;position:fixed;text-align:center;font-size:0;bottom:0;left:0;right:0;margin:auto">
       <label for="aadsstickyme5l1sdx" style="top: 50%;transform: translateY(-50%);right:24px;; position: absolute;border-radius: 4px; background: rgba(248, 248, 249, 0.70); padding: 4px;z-index: 99999;cursor:pointer">
         <svg fill="#000000" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
           <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "/>
