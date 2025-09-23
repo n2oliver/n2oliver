05724dc9f28f91c3fbe0c0e19ce4ae2b99f50d9d
n2oliver <silva.liver@gmail.com>
2025-09-23 19:37:14 -0300

    ajuste no iframe de anuncio do aads

--- jogos/index.php
+++ jogos/index.php
@@ -239,7 +239,7 @@ $APP_URL = '/jogos'; ?>
       </div>
   </div>
   
-      <div id="frame" class="frame" style="width: 100%;margin: auto;position: relative !important; background: rgba(0, 0, 0, 0.50); z-index: 9;">
+      <div class="frame" style="width: 100%;margin: auto;position: relative !important; background: rgba(0, 0, 0, 0.50); z-index: 9;">
       <iframe data-aa='2410752' src='//acceptable.a-ads.com/2410752/?size=Adaptive'
                           style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
       </div>
