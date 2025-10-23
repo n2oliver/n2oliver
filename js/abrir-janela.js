c8b6334369cf48103c16bb810bfff5fca4b73aa2
n2oliver <silva.liver@gmail.com>
2025-10-23 18:20:35 -0300

    atualiza método de abertura de nova janela

--- js/abrir-janela.js
+++ js/abrir-janela.js
@@ -1,8 +1,8 @@
-function abrirJanela(url,offWindowUrl) {
+function abrirJanela(url,offWindowUrl,parameters={}) {
   window.open(
     url, // URL
     '_blank',         // nome da janela
-    'width='+screen.availWidth + ',height=' + screen.availHeight + ',menubar=no,toolbar=no,location=no,status=no,resizable=yes,scrollbars=yes,noopener,noreferrer'
+    !parameters.showMenu ? 'width='+screen.availWidth + ',height=' + screen.availHeight + ',resizable=yes,scrollbars=yes'+',noopener,noreferrer,location=no,status=no' : null
   );
   
   if(offWindowUrl) {
