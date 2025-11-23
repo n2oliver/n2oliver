272030e6078a0dafeebdf5225588f43dc620af84
n2oliver <silva.liver@gmail.com>
2025-11-23 06:36:56 -0300

    melhora posição de banner aads

--- jogos/index.php
+++ jogos/index.php
@@ -224,6 +224,24 @@ if ($impressionid) {
   <script src="/js/bootbox/bootbox.min.js"  defer></script>
   <div class="col-md-10 m-auto" style="position: sticky; top: 0; z-index: 999;">
     <?php include('../navbar.php'); ?>
+    <div style="position: absolute; z-index: 99999">
+      <input autocomplete="off" type="checkbox" id="aadsstickymibirl03" hidden />
+      <div style="padding-top: auto; padding-bottom: 0;">
+        <div style="width:100%;height:auto;position:fixed;text-align:center;font-size:0;top:0;left:0;right:0;margin:auto">
+          <label for="aadsstickymibirl03" style="top: 50%;transform: translateY(-50%);right:24px;; position: absolute;border-radius: 4px; background: rgba(248, 248, 249, 0.70); padding: 4px;z-index: 99999;cursor:pointer">
+            <svg fill="#000000" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
+              <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "/>
+            </svg>
+          </label>
+          <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;"><iframe data-aa=2410752 src=//acceptable.a-ads.com/2410752/?size=Adaptive style='border:0; padding:0; width:70%; height:auto; overflow:hidden; margin: auto'></iframe></div>
+        </div>
+        <style>
+        #aadsstickymibirl03:checked + div {
+          display: none;
+        }
+      </style>
+      </div>
+    </div>
   </div>
     
   <header class="mx-auto col-md-10 text-center mt-1">
@@ -283,10 +301,6 @@ if ($impressionid) {
     </section>
 
     <div id="progressbar"></div>
-    <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 1;">
-      <iframe data-aa='2410752' src='//acceptable.a-ads.com/2410752/?size=Adaptive'
-                        style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
-    </div>
     <div class="container m-auto p-0 mt-1">
       <div class="donation-section m-0 row">
         <div class="col-md-6">
