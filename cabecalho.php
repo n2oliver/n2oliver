b70807d58b59e9e494b6820d1803dff353789383
n2oliver <silva.liver@gmail.com>
2025-12-10 15:07:40 -0300

    remove ads google

--- cabecalho.php
+++ cabecalho.php
@@ -73,5 +73,3 @@
             }
         }
         </script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8124594027670911"
-     crossorigin="anonymous"></script>
