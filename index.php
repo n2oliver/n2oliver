920cc81b89cc011a66f10c8f9e6acdc6c2271f8c
n2oliver <silva.liver@gmail.com>
2025-12-18 22:09:49 -0300

    melhoria da experiência de usuário

--- index.php
+++ index.php
@@ -180,39 +180,41 @@
                 </div>
             </header>
 
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0 mx-0 w-100 m-auto px-0"
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0 mx-0 w-100 m-auto px-0" 
                 style="background-image: url(/img/theme-image-natal.png);">
                 <div class="container-fluid">
 
+                <div class="d-flex w-100 justify-content-between">
                     <!-- Logo -->
                     <a class="navbar-brand rounded" href="/">
-                        <img src="/img/n2-ico.jpg" width="32" class="rounded" alt="logo">
-                        oliver
+                    <img src="/img/n2-ico.jpg" width="32" class="rounded" alt="logo">oliver
                     </a>
 
-                    <!-- GTranslate wrapper visualmente à direita -->
-                    <div class="gtranslate_wrapper ms-auto me-2 d-flex align-items-center"></div>
-
+                
                     <!-- Botão hamburger -->
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
                     <!-- Navbar links -->
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item"><a class="nav-link" href="/"><i class="fa-solid fa-home"></i> Início</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/noticias.php"><i class="fa-solid fa-newspaper"></i> Blog</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/noticias.php"><i class="fa-solid fa-newspaper"></i> Notícias</a></li>
                             <li class="nav-item"><a class="nav-link" href="/aplicativos.php"><i class="fa-solid fa-hand-pointer"></i> Aplicativos</a></li>
                             <li class="nav-item"><a class="nav-link" href="/jogos/index.php"><i class="fa-solid fa-puzzle-piece"></i> Jogos</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/lojavirtual/index.php"><i class="fa-solid fa-store"></i> Oliv3r Store</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/lojavirtual/index.php"><i class="fa-solid fa-store"></i> Loja Virtual</a></li>
                             <li class="nav-item"><a class="nav-link" href="/contato.php"><i class="fa-solid fa-envelope"></i> Contato</a></li>
                         </ul>
                     </div>
+                </div>
+
+                    <!-- GTranslate wrapper visualmente à direita -->
+                    <div class="gtranslate_wrapper ms-auto me-2 d-flex align-items-center"></div>
+
 
                 </div>
-            </nav>
+                </nav>
             <main>
                 <div class="mt-2 d-flex about-me mb-2" style="height: auto; flex-direction: row">
                     <div class="container d-flex m-0 quicksand main-content" style="border-bottom-left-radius: 0px; height: fit-content">
--- navbar.php
+++ navbar.php
@@ -2,21 +2,18 @@
 style="background-image: url(/img/theme-image-natal.png);">
   <div class="container-fluid">
 
+  <div class="d-flex w-100 justify-content-between">
     <!-- Logo -->
     <a class="navbar-brand rounded" href="/">
-      <img src="/img/n2-ico.jpg" width="32" class="rounded" alt="logo">
-      oliver
+      <img src="/img/n2-ico.jpg" width="32" class="rounded" alt="logo">oliver
     </a>
 
-    <!-- GTranslate wrapper visualmente à direita -->
-    <div class="gtranslate_wrapper ms-auto me-2 d-flex align-items-center"></div>
-
+  
     <!-- Botão hamburger -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
     <!-- Navbar links -->
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
@@ -28,6 +25,11 @@ style="background-image: url(/img/theme-image-natal.png);">
             <li class="nav-item"><a class="nav-link" href="/contato.php"><i class="fa-solid fa-envelope"></i> Contato</a></li>
         </ul>
     </div>
+</div>
+
+    <!-- GTranslate wrapper visualmente à direita -->
+    <div class="gtranslate_wrapper ms-auto me-2 d-flex align-items-center"></div>
+
 
   </div>
 </nav>
