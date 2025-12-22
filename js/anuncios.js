682fbea2091470b03640d8ba75affdea249349b3
n2oliver <silva.liver@gmail.com>
2025-12-22 19:05:00 -0300

    adicionando inteligência de abertura de link

--- js/anuncios.js
+++ js/anuncios.js
@@ -1,10 +1,12 @@
 const SMARTLINK_1 = 'https://laxativethem.com/vs23jmys5q?key=7c2ccbc5de27850e97ac9aae68ac23a4';
 const SMARTLINK_2 = 'https://directads.adclickppc.com/dl/?16925b62-e818-4353-8bb6-0fe491d50746';
+const params1 = 'width='+ ((screen.availWidth / 2).toFixed(0)) + ',height=' + ((screen.availHeight / 2).toFixed(0)) + ',resizable=yes,scrollbars=yes';
+const params2 = 'width='+ ((screen.availWidth / 3).toFixed(0)) + ',height=' + ((screen.availHeight / 3).toFixed(0)) + ',resizable=yes,scrollbars=yes';
+
 let anuncioAbriu;
+
 document.addEventListener('DOMContentLoaded', () => {
   if (window.location.href.indexOf('utm_source=popads') != -1 && !sessionStorage.getItem('smartlink_aberto')) {
-    const params1 = 'width='+ ((screen.availWidth / 2).toFixed(0)) + ',height=' + ((screen.availHeight / 2).toFixed(0)) + ',resizable=yes,scrollbars=yes';
-    const params2 = 'width='+ ((screen.availWidth / 3).toFixed(0)) + ',height=' + ((screen.availHeight / 3).toFixed(0)) + ',resizable=yes,scrollbars=yes';
     anuncioAbriu = window.open(SMARTLINK_2, '_blank', params2);
     window.open(SMARTLINK_1, '_blank', params1);
     if(anuncioAbriu) {
@@ -40,7 +42,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setTimeout(() => {
       if(window.location.href.indexOf('utm_source=popads') != -1 && !sessionStorage.getItem('smartlink_aberto') && !anuncioAbriu) {
-        window.location.href = SMARTLINK_1;
+        anuncioAbriu = window.open(SMARTLINK_2, '_blank', params2);
+        window.open(SMARTLINK_1, '_blank', params1);
+        if(anuncioAbriu) {
+          sessionStorage.setItem('smartlink_aberto', '1');
+        } else {
+          window.location.href = SMARTLINK_1;
+        }
       }
       $('#ads').slideUp("slow", ()=>{
         $(this).hide();
