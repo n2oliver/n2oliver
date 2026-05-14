b4f70cd61c93422e77dfe7d6b18817b4a3ea988d
n2oliver <silva.liver@gmail.com>
2026-05-14 13:32:14 -0300

    troca smartlink

--- js/anuncios.js
+++ js/anuncios.js
@@ -1,4 +1,4 @@
-const SMARTLINK_1 = 'https://laxativethem.com/c3vcjhu7x?key=791e383fa6c2cf2f88b6b88d66f02cc2';
+const SMARTLINK_1 = 'https://laxativethem.com/mh1i2a63pp?key=c2d2b12edb1f9e816660ba00c16a4b1b';
 const SMARTLINK_2 = 'https://otieu.com/4/10285691';
 const SMARTLINK_3 = 'https://directads.adclickppc.com/dl/?16925b62-e818-4353-8bb6-0fe491d50746';
 const params1 = 'width='+ ((screen.availWidth / 2).toFixed(0)) + ',height=' + ((screen.availHeight / 2).toFixed(0)) + ',resizable=yes,scrollbars=yes';
