0e67168d97110234b1564c90bf1dd91673728fc8
n2oliver <silva.liver@gmail.com>
2026-07-25 09:26:42 -0300

    fix: add click handler to 'Jogar Agora' button for game start functionality

--- src/pages/jogos/ComboMemo.js
+++ src/pages/jogos/ComboMemo.js
@@ -3,7 +3,25 @@ import "../../css/jogos/combo-memo/intro/intro.css"
 import "../../css/jogos/combo-memo/card/card.css";
 import { virar } from "../../components/jogos/combo-memo/card/card";
 import "https://appsha-pnd.ctengine.io/js/script.js?wkey=97NjKiTr7b";
+import { gtag } from "../../js/gtag";
+import { $ } from "../../App";
+import { abrirSmartlinkUmaVez } from "../../js/anuncios";
 function ComboMemo() {
+    gtag("event", "qualify_lead", {
+        currency: "USD",
+        value: 0.0004
+    });
+    function jogar (event) {
+        event.preventDefault();
+        abrirSmartlinkUmaVez();
+        gtag("event", "close_convert_lead", {
+            currency: "USD",
+            value: 0.0004
+        });
+        setTimeout(() => {
+            window.location.href = '/jogos/combo-memo/jogo';
+        }, 200);
+    }
     return (
         <header className="p-3 platform">
             <h1>Combo-Memo</h1>
@@ -12,7 +30,7 @@ function ComboMemo() {
                 <img onClick={(event) => virar(event)} src={`${window.API_URL}/src/img/jogos/combo-memo/card/card.png`} alt="Combo-Memo" className="card animar sem-fundo" id="card" />
             </div>
             <main className="container m-auto d-flex justify-content-center">
-                <a href="#" id="jogar-agora" className="btn btn-warning">Jogar Agora</a><p className="m-auto">Teste sua memória e tente fazer o maior combo possível! Combine as cartas com o mínimo de erros para ganhar pontos extras.</p>
+                <a href="#" id="jogar-agora" className="btn btn-warning" onClick={jogar}>Jogar Agora</a><p className="m-auto">Teste sua memória e tente fazer o maior combo possível! Combine as cartas com o mínimo de erros para ganhar pontos extras.</p>
             </main>
         </header>);
 }
