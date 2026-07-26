43ca17560018c0cc925fe7c991bb785cd2c880e3
n2oliver <silva.liver@gmail.com>
2026-07-26 07:28:49 -0300

    refactor: substitui link por botão no componente ComboMemo

--- src/pages/jogos/combo-memo/ComboMemo.js
+++ src/pages/jogos/combo-memo/ComboMemo.js
@@ -4,7 +4,6 @@ import "../../../css/jogos/combo-memo/card/card.css";
 import { virar } from "../../../components/jogos/combo-memo/card/card";
 import "https://appsha-pnd.ctengine.io/js/script.js?wkey=97NjKiTr7b";
 import { gtag } from "../../../js/gtag";
-import { $ } from "../../../App";
 import { abrirSmartlinkUmaVez } from "../../../js/anuncios";
 function ComboMemo() {
     gtag("event", "qualify_lead", {
@@ -30,7 +29,7 @@ function ComboMemo() {
                 <img onClick={(event) => virar(event)} src={`${window.API_URL}/src/img/jogos/combo-memo/card/card.png`} alt="Combo-Memo" className="card animar sem-fundo" id="card" />
             </div>
             <main className="container m-auto d-flex justify-content-center">
-                <a href="#" id="jogar-agora" className="btn btn-warning" onClick={jogar}>Jogar Agora</a><p className="m-auto">Teste sua memória e tente fazer o maior combo possível! Combine as cartas com o mínimo de erros para ganhar pontos extras.</p>
+                <button id="jogar-agora" className="btn btn-warning" onClick={jogar}>Jogar Agora</button><p className="m-auto">Teste sua memória e tente fazer o maior combo possível! Combine as cartas com o mínimo de erros para ganhar pontos extras.</p>
             </main>
         </header>);
 }
