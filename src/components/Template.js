f1667e128ae305d9304ac999f3207f6d6e0e4276
n2oliver <silva.liver@gmail.com>
2026-07-27 19:32:39 -0300

    refactor: corrige o ID do div principal no componente Template e ajusta estilos no games.css

--- src/components/Template.js
+++ src/components/Template.js
@@ -7,7 +7,7 @@ function Template() {
     return (
         <section id="template" className="w-100 m-auto n2oliver-jogos d-flex flex-column justify-content-center"
             alt="">
-            <div id="games" className="container m-auto p-0">
+            <div id="template-main-div" className="container m-auto p-0">
                 <div id="template-details" className="flex-row px-0 col-md-10">
                     <div className="w-100">
                         <div className="w-100 row justify-content-around">
