30ef50a6689998db5bd8c67a52b89ac0c6a339d6
n2oliver <silva.liver@gmail.com>
2026-07-28 22:30:22 -0300

    feat: adiciona tela de jogo de caca-plavras

---
+++ src/components/jogos/caca-palavras/jogar/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="col-md-10">
+            <h1>Caça-Palavras</h1>
+            <p>Arraste para selecionar as palavras.</p>
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/jogos/caca-palavras/jogar/CacaPalavras.js
+++ src/pages/jogos/caca-palavras/jogar/CacaPalavras.js
@@ -1,5 +1,15 @@
+import Header from "../../../../components/jogos/caca-palavras/jogar/Header";
+import Main from "../../../../components/jogos/caca-palavras/jogar/Main";
+import Message from "../../../../components/jogos/caca-palavras/jogar/Message";
+import "../../../../css/jogos/caca-palavras/jogar/style.css";
 function CacaPalavras() {
-    return (<></>)
+    return (
+        <>
+            <Header />
+            <Main />
+            <Message />
+        </>
+    )
 }
 
 export default CacaPalavras;
