82dd1eb79e4ad632cc3617b0c4090a1c475c7b3f
n2oliver <silva.liver@gmail.com>
2026-08-07 09:21:59 -0300

    atualiza arquivos

--- src/pages/jogos/linha-amarela/Jogo.jsx
+++ src/pages/jogos/linha-amarela/Jogo.jsx
@@ -1,6 +1,5 @@
 import GameSound from "../../../components/jogos/linha-amarela/GameSound";
 import BGTransparent from "../../../components/jogos/linha-amarela/Jogo/BGTransparent";
-<<<<<<< HEAD
 import GameOver from "../../../components/jogos/linha-amarela/Jogo/GameOver";
 import Level from "../../../components/jogos/linha-amarela/Jogo/Level";
 import Menu from "../../../components/jogos/linha-amarela/Jogo/Menu";
@@ -15,24 +14,15 @@ import "../../../css/jogos/linha-amarela/game.css";
 import "../../../css/jogos/linha-amarela/intro.css";
 import "../../../css/jogos/linha-amarela/style.css";
 
-=======
-import Menu from "../../../components/jogos/linha-amarela/Jogo/Menu";
-import Spinner from "../../../components/jogos/linha-amarela/Spinner";
-
->>>>>>> d064d49 (feat: início da construção da tela de jogo linha-amarela)
 function Jogo() {
     return (<>
         <Spinner />
         <GameSound />
         <BGTransparent />
         <Menu />
-<<<<<<< HEAD
         <Markers />
         <Level />
         <GameOver />
     </>);
-=======
-    </>)
->>>>>>> d064d49 (feat: início da construção da tela de jogo linha-amarela)
 }
 export default Jogo;
