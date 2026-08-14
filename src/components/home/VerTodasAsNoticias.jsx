3ea7830c92738cee852560e7c8b23a2f606e2e17
n2oliver <silva.liver@gmail.com>
2026-08-14 06:33:56 -0300

    feat: integração stay22

--- src/components/home/VerTodasAsNoticias.jsx
+++ src/components/home/VerTodasAsNoticias.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function VerTodasAsNoticias() {
     return (
-        <div className="container m-auto p-0 mt-1 col-md-10 d-flex justify-content-end">
+        <div className="m-auto p-0 mt-1 col-md-10 d-flex justify-content-end">
             <Link to="/noticias"><button className="btn btn-primary">Ver todas as notícias</button></Link>
         </div>
     );
